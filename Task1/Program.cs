1856753c5e0c8391724ccc375522de345e5b11b4
Alexey <pwz-and_10@mail.ru>
2022-08-09 00:09:19 +0600

    Modding Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число:");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (!input.Equals("q"))
+                Console.WriteLine("{0}->{1}", input, input[1]);
+            else
+                break;
+        }
+    }
+}

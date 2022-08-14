bbdedb0c94f5631f219fc3fc83df46f42bfc2e26
Alexey <pwz-and_10@mail.ru>
2022-08-14 17:01:33 +0600

    Create Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine($"Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(number);
+if(NumberText.Length > 2)
+{
+    Console.WriteLine(NumberText[2]);
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}

e86bebb64d2e1245cbfb5305027377c54ffd000e
Alexey <pwz-and_10@mail.ru>
2022-08-14 16:59:26 +0600

    Create Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,14 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Введите трехзначное число:");
+        Console.WriteLine($"Введите трехзначное число:");
         while (true)
         {
             string input = Console.ReadLine();
             if (!input.Equals("q"))
-                Console.WriteLine("{0}->{1}", input, input[1]);
+            Console.WriteLine("{0}->{1}", input, input[1]);
             else
-                break;
+            break;
         }
     }
 }

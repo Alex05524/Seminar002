f947c3bcc53a2b4dc656d1cb2ca771c4dfd5491d
Alexey <pwz-and_10@mail.ru>
2022-08-14 17:05:01 +0600

    Create Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine($"Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a == 6 || a == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
